53b6ddf569ba237868f1899bdff72e01770cf33e
nopriterbang <novalpriya@gmail.com>
2024-05-31 15:33:10 +0700

    menambahkan void insert

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -10,7 +10,31 @@ public:
 	FRONT = -1;
 	REAR = -1;
 	}
-	void
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		// Cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// Jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
 
 
 
