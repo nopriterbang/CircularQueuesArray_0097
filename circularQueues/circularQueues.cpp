fc003948b0cec403b35abf76d0e0e3b2436d6b78
nopriterbang <novalpriya@gmail.com>
2024-05-31 14:51:50 +0700

    cek apakah antrian kosong

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -4,4 +4,10 @@ using namespace std;
 void display() {
 	int FRONT_position = FRONT;
 	int REAR_position = REAR;
+
+	// Cek apakah antrian kosong
+	if (FRONT == 1) {
+		cout << "Queues is empty\n";
+		return;
+	}
 }
