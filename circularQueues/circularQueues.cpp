3d0cea04265cf8c2840b95a275668cb9739b74aa
nopriterbang <novalpriya@gmail.com>
2024-05-31 15:08:28 +0700

    membuat acces modifier dan set FRONT, REAR = -1

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -5,6 +5,12 @@ class Queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
 
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
 }
 
 void display() {
@@ -50,3 +56,8 @@ void display() {
 
 
 };
+
+int main() {
+	Queues q;
+	char ch;
+}
