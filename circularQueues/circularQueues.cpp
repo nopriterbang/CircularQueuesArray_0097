8d52aa28bea344338522199c3e9ebf9a12c8ec72
nopriterbang <novalpriya@gmail.com>
2024-05-31 15:10:36 +0700

    menambahkan while bernilai true

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -2,40 +2,40 @@
 using namespace std;
 
 class Queues {
-	int FRONT, REAR, max = 5;
-	int queue_array[5];
+		int FRONT, REAR, max = 5;
+		int queue_array[5];
 
-public:
-	Queues() {
+	public:
+		Queues() {
 		FRONT = -1;
 		REAR = -1;
-	}
+		}
 
-}
 
-void display() {
+
+	void display() {
 	int FRONT_position = FRONT;
 	int REAR_position = REAR;
 
-	// Cek apakah antrian kosong
-	if (FRONT == 1) {
-		cout << "Queues is empty\n";
-		return;
-	}
+		// Cek apakah antrian kosong
+		if (FRONT == 1) {
+			cout << "Queues is empty\n";
+			return;
+		}
 
-	cout << "\nElements in queue are...\n";
+		cout << "\nElements in queue are...\n";
 
-	// Jika FRONT_position <== REAR_position, iterasi dari FRONT hingga REAR
-	if (FRONT_position <= REAR_position) {
+		// Jika FRONT_position <== REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
 		while (FRONT_position <= REAR_position) {
 			cout << "queue_array[FRONT_position]" << " ";
 			FRONT_position++;
 		}
 		cout << endl;
 
-	}
-	else
-	{
+		}
+		else
+		{
 		//jika FRONT > REAR, iterasi dari FRONT hingga akhir array
 		while (FRONT_Position <= max - 1)
 		{
@@ -60,4 +60,12 @@ void display() {
 int main() {
 	Queues q;
 	char ch;
+}
+
+while (true) {
+
+
+
+
+
 }
